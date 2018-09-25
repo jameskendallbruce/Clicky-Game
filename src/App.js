969ba310198b1353bb52c1f4c364f48bf3312311
jameskendallbruce <jameskendallbruce@gmail.com>
2018-09-25 14:43:49 -0400

    Actually working
    Added functionality. Too hard though. :/

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import React from "react";
 import GameCard from "./components/GameCard";
 import Wrapper from "./components/Wrapper";
-import colorsAPI from "./colorsAPI.json";
+import colorsArr from "./colorsArr.json";
 import "./App.css";
+import easyArr from "./easyArr.json"
 
 import logo from './logo.svg';
 
-const scoreNow = 0;
-
-const scoreBest = 0;
+function randomCards(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+      return array;
+};
 
 class App extends  React.Component {
 
   // This is important to keep our json the same but allow people to alter an individual "instance" of our friends array.
   state = {
-    colorsAPI
-  }
+    colorsArr,
+    easyArr,
+    scoreNow: 0,
+    scoreBest: 0,
+    EndScreen: "",
+    clicked: [],
+  };
 
-  erase = (key) => {
-    this.setState({ colorsAPI: this.state.colorsAPI.filter(color => color.id !== key ) })
-  }
+  clickMe = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.updateScore();
+      // changing the value of "clicked" in the state array for this entry in the ColorsArr
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.reset();
+    }
+  };
+
+  updateScore = () => {
+    const newScore = this.state.scoreNow + 1;
+    this.setState({
+      scoreNow: newScore,
+      EndScreen: ""
+    });
+    if (newScore >= this.state.scoreBest) {
+      this.setState({ scoreBest: newScore });
+    }
+    if (newScore === this.state.colorsArr.length) {
+      this.setState({ EndScreen: "You win!" });
+    }
+    this.shuffleArr();
+  };
+
+  // resets the entire page. State is updated and the shuffle function is called.
+  reset = () => {
+    this.setState({
+      scoreNow: 0,
+      scoreBest: this.state.scoreBest,
+      EndScreen: "You already guessed that!",
+      clicked: []
+    });
+    this.shuffleArr();
+  };
+
+  // shuffles the entries in the colorsArr and updates it in the state.
+  shuffleArr = () => {
+    let shuffledArray = randomCards(this.state.colorsArr);
+    this.setState({ CardsAPI: shuffledArray });
+  };
+
 
   render(){
     return (
@@ -30,17 +79,16 @@ class App extends  React.Component {
           <div className="row">
             <div className="col-5 align-middle">
               <h1 className="row" id="clickyGame">Clicky Game</h1>
-              {/* <img src={logo} className="App-logo row" alt="logo"/> */}
+              <h3 className="row instructions end">{this.state.EndScreen}</h3>
             </div>
             <div className="col-1">
               <img src={logo} className="App-logo row" alt="logo"/>
             </div>
-            {/* <br className="col-6"></br> */}
             <div className="col-4 offset-2 instructions">
               <h2 className="App-title">Click each color only once.</h2> 
               <h2 className="App-title">Each correct color is 1 point.</h2>
-              <h3 className="score" id="currentScore">Score: {scoreNow}</h3>
-              <h3 className="score" id="highScore">High Score: {scoreBest}</h3>
+              <h3 className="score" id="currentScore">Score: {this.state.scoreNow}</h3>
+              <h3 className="score" id="highScore">High Score: {this.state.scoreBest}</h3>
             </div>
           </div>
         </div>
@@ -48,10 +96,15 @@ class App extends  React.Component {
       <Wrapper>
         <div className="container">
           <div className="row">
-            {this.state.colorsAPI.map(color => (
+            {this.state.colorsArr.map(color => (
               <GameCard
                 image={color.image}
                 key={color.id}
+                clickMe={this.clickMe}
+                updateScore={this.updateScore}
+                reset={this.reset}
+                shuffleArr={this.shuffleArr}
+                playEasy={this.playEasy}
                 id = {color.id}
               />
             ))}
--- src/components/GameCard/GameCard.js
+++ src/components/GameCard/GameCard.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./GameCard.css";
 
 const GameCard = props => (
-  <div className="card mx-auto">
+  <div className="card mx-auto" value={props.id} onClick={() => props.clickMe(props.id)}>
     <div>
-      <img className="mx-auto align-middle" alt={props.image} src={props.image} />
+      <img className="imgCard" alt={props.image} src={props.image} />
     </div>
   </div>
 );
